23d5d3a4f5c632840d462225cf220f49400a4091
Kotaro Abe <abekoh.bcky@gmail.com>
2024-02-25 19:03:55 +0900

    test: refactor

--- internal/log/log_test.go
+++ internal/log/log_test.go
@@ -34,4 +34,31 @@ func TestManager(t *testing.T) {
 			t.Errorf("expected [abcd], got %v", logs)
 		}
 	})
+	t.Run("Append many", func(t *testing.T) {
+		t.Parallel()
+		fm, err := file.NewManager(t.TempDir(), 128)
+		if err != nil {
+			t.Fatal(err)
+		}
+		lm, err := NewManager(fm, "logfile")
+		if err != nil {
+			t.Fatal(err)
+		}
+		for i := 0; i < 10000; i++ {
+			_, err := lm.Append([]byte("abcd"))
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+		count := 0
+		for r := range lm.Iterator() {
+			count++
+			if string(r) != "abcd" {
+				t.Errorf("expected abcd, got %s", string(r))
+			}
+		}
+		if count != 10000 {
+			t.Errorf("expected 10000, got %d", count)
+		}
+	})
 }
