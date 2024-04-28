5278e291518e8787add03753ff9f2569539b6e3f
Kotaro Abe <abekoh.bcky@gmail.com>
2024-04-28 09:57:46 +0900

    feat: interface

---
+++ internal/query/query.go
@@ -0,0 +1,24 @@
+package query
+
+import "github.com/abekoh/simple-db/internal/record"
+
+type Scan interface {
+	BeforeFirst() error
+	Next() (bool, error)
+	Int32(fieldName string) (int32, error)
+	Str(fieldName string) (string, error)
+	Val(fieldName string) (any, error)
+	HasField(fieldName string) (bool, error)
+	Close() error
+}
+
+type UpdateScan interface {
+	Scan
+	SetVal(fieldName string, val any) error
+	SetInt32(fieldName string, val int32) error
+	SetStr(fieldName string, val string) error
+	Insert() error
+	Delete() error
+	RID() (record.RID, error)
+	MoveToRID(rid record.RID) error
+}
