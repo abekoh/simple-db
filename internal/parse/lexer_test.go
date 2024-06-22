6702ae8c8e7706474f6de75ed9d3bd1ebbf7fa0d
Kotaro Abe <abekoh.bcky@gmail.com>
2024-06-22 13:14:02 +0900

    refactor

--- internal/parse/lexer_test.go
+++ internal/parse/lexer_test.go
@@ -3,9 +3,6 @@ package parse
 import (
 	"reflect"
 	"testing"
-
-	"github.com/abekoh/simple-db/internal/query"
-	"github.com/abekoh/simple-db/internal/record/schema"
 )
 
 func TestLexer(t *testing.T) {
@@ -118,42 +115,6 @@ func TestLexer(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("got %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestParser_Query(t *testing.T) {
-	tests := []struct {
-		name    string
-		s       string
-		want    *QueryData
-		wantErr bool
-	}{
-		{
-			name: "SELECT",
-			s:    "SELECT a, b FROM mytable WHERE a = 1 AND b = 'foo'",
-			want: &QueryData{
-				fields: []string{"a", "b"},
-				tables: []string{"mytable"},
-				pred: query.Predicate{
-					query.NewTerm(schema.FieldName("a"), schema.ConstantInt32(1)),
-					query.NewTerm(schema.FieldName("b"), schema.ConstantStr("foo")),
-				},
-			},
-			wantErr: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			p := NewParser(tt.s)
-			got, err := p.Query()
-			if (err != nil) != tt.wantErr {
-				t.Errorf("Query() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Query() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
