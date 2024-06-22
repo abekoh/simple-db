36a4d8b463a63e7c31678d80f96179cd9cd879ff
Kotaro Abe <abekoh.bcky@gmail.com>
2024-06-23 00:12:21 +0900

    add interface

--- internal/plan/plan.go
+++ internal/plan/plan.go
@@ -1,8 +1,10 @@
 package plan
 
 import (
+	"github.com/abekoh/simple-db/internal/parse"
 	"github.com/abekoh/simple-db/internal/query"
 	"github.com/abekoh/simple-db/internal/record/schema"
+	"github.com/abekoh/simple-db/internal/transaction"
 )
 
 type Plan interface {
@@ -11,4 +13,17 @@ type Plan interface {
 	RecordsOutput() (int, error)
 	DistinctValues(fieldName string) (int, error)
 	Schema() *schema.Schema
+}
+
+type QueryPlanner interface {
+	CreatePlan(d *parse.QueryData, tx *transaction.Transaction) (Plan, error)
+}
+
+type UpdatePlanner interface {
+	ExecuteInsert(d *parse.InsertData, tx *transaction.Transaction) (int, error)
+	ExecuteDelete(d *parse.DeleteData, tx *transaction.Transaction) (int, error)
+	ExecuteModify(d *parse.ModifyData, tx *transaction.Transaction) (int, error)
+	ExecuteCreateTable(d *parse.CreateTableData, tx *transaction.Transaction) (int, error)
+	ExecuteCreateView(d *parse.CreateViewData, tx *transaction.Transaction) (int, error)
+	ExecuteCreateIndex(d *parse.CreateIndexData, tx *transaction.Transaction) (int, error)
 }
