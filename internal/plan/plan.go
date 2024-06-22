3fa0190bc936157efb4373a2338a7d3a85f92545
Kotaro Abe <abekoh.bcky@gmail.com>
2024-06-23 00:32:39 +0900

    fix plan

--- internal/plan/plan.go
+++ internal/plan/plan.go
@@ -1,6 +1,8 @@
 package plan
 
 import (
+	"fmt"
+
 	"github.com/abekoh/simple-db/internal/parse"
 	"github.com/abekoh/simple-db/internal/query"
 	"github.com/abekoh/simple-db/internal/record/schema"
@@ -44,4 +46,27 @@ func (p *Planner) CreateQueryPlan(q string, tx *transaction.Transaction) (Plan, 
 		return nil, err
 	}
 	return p.qp.CreatePlan(qd, tx)
+}
+
+func (p *Planner) ExecuteUpdate(q string, tx *transaction.Transaction) (int, error) {
+	ps := parse.NewParser(q)
+	cmd, err := ps.UpdateCommand()
+	if err != nil {
+		return 0, err
+	}
+	switch c := cmd.(type) {
+	case *parse.InsertData:
+		return p.up.ExecuteInsert(c, tx)
+	case *parse.DeleteData:
+		return p.up.ExecuteDelete(c, tx)
+	case *parse.ModifyData:
+		return p.up.ExecuteModify(c, tx)
+	case *parse.CreateTableData:
+		return p.up.ExecuteCreateTable(c, tx)
+	case *parse.CreateViewData:
+		return p.up.ExecuteCreateView(c, tx)
+	case *parse.CreateIndexData:
+		return p.up.ExecuteCreateIndex(c, tx)
+	}
+	return 0, fmt.Errorf("unknown command type %v", cmd)
 }
