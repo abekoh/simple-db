edfa14dfcec6a59cadb01b3e511b7a01cc50ab4c
Kotaro Abe <abekoh.bcky@gmail.com>
2024-06-23 22:50:14 +0900

    feat: TablePlan

--- internal/plan/plan.go
+++ internal/plan/plan.go
@@ -1,14 +1,59 @@
 package plan
 
 import (
+	"github.com/abekoh/simple-db/internal/metadata"
 	"github.com/abekoh/simple-db/internal/query"
+	"github.com/abekoh/simple-db/internal/record"
 	"github.com/abekoh/simple-db/internal/record/schema"
+	"github.com/abekoh/simple-db/internal/transaction"
 )
 
 type Plan interface {
 	Open() (query.Scan, error)
-	BlockAccessed() (int, error)
-	RecordsOutput() (int, error)
-	DistinctValues(fieldName string) (int, error)
+	BlockAccessed() int
+	RecordsOutput() int
+	DistinctValues(fieldName schema.FieldName) int
 	Schema() *schema.Schema
+}
+
+type TablePlan struct {
+	tableName string
+	tx        *transaction.Transaction
+	layout    *record.Layout
+	statInfo  metadata.StatInfo
+}
+
+var _ Plan = (*TablePlan)(nil)
+
+func NewTablePlan(
+	tableName string,
+	tx *transaction.Transaction,
+	layout *record.Layout,
+	statInfo metadata.StatInfo) *TablePlan {
+	return &TablePlan{
+		tableName: tableName,
+		tx:        tx,
+		layout:    layout,
+		statInfo:  statInfo,
+	}
+}
+
+func (t TablePlan) Open() (query.Scan, error) {
+	return record.NewTableScan(t.tx, t.tableName, t.layout)
+}
+
+func (t TablePlan) BlockAccessed() int {
+	return t.statInfo.BlocksAccessed()
+}
+
+func (t TablePlan) RecordsOutput() int {
+	return t.statInfo.RecordsOutput()
+}
+
+func (t TablePlan) DistinctValues(fieldName schema.FieldName) int {
+	return t.statInfo.DistinctValues(fieldName)
+}
+
+func (t TablePlan) Schema() *schema.Schema {
+	return t.layout.Schema()
 }
