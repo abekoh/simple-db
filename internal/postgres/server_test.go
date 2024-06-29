bf28dc1f146bf16bc5025882ba3927877effded8
Kotaro Abe <abekoh.bcky@gmail.com>
2024-06-29 16:05:08 +0900

    fix

--- internal/postgres/server_test.go
+++ internal/postgres/server_test.go
@@ -11,17 +11,17 @@ import (
 func TestPostgres(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	cfg := Config{
-		Dir:     t.TempDir(),
-		Address: "127.0.0.1:54329",
-	}
-	go func() {
-		if err := RunServer(ctx, cfg); err != nil {
-			t.Error(err)
-		}
-	}()
-
-	conn, err := pgx.Connect(ctx, "postgres://127.0.0.1:54329/postgres")
+	//cfg := Config{
+	//	Dir:     t.TempDir(),
+	//	Address: "127.0.0.1:54329",
+	//}
+	//go func() {
+	//	if err := RunServer(ctx, cfg); err != nil {
+	//		t.Error(err)
+	//	}
+	//}()
+	//
+	conn, err := pgx.Connect(ctx, "postgres://postgres@127.0.0.1:5432/postgres")
 	if err != nil {
 		t.Fatal(err)
 	}
