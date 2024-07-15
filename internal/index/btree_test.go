e6edc26e3db4bfee8bfe143db9c6943711698650
Kotaro Abe <abekoh.bcky@gmail.com>
2024-07-15 12:36:21 +0900

    tset: update

--- internal/index/btree_test.go
+++ internal/index/btree_test.go
@@ -29,41 +29,62 @@ func TestNewBTreeIndex_OneIndex(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if err := ts.Insert(); err != nil {
-		t.Fatal(err)
-	}
-	if err := ts.SetVal("A", schema.ConstantStr("aaa")); err != nil {
-		t.Fatal(err)
-	}
-
 	idxLayout := index.NewIndexLayout(field)
 	idx, err := index.NewBTreeIndex(tx, "myindex", idxLayout)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if err := idx.Insert(schema.ConstantStr("aaa"), ts.RID()); err != nil {
-		t.Fatal(err)
+	vals := make([]schema.Constant, 0)
+	for c := 'a'; c <= 'z'; c++ {
+		vals = append(vals, schema.ConstantStr(c))
+	}
+	for c := 'A'; c <= 'Z'; c++ {
+		vals = append(vals, schema.ConstantStr(c))
+	}
+	for c := '0'; c <= '9'; c++ {
+		vals = append(vals, schema.ConstantStr(c))
 	}
 
-	if err := idx.BeforeFirst(schema.ConstantStr("aaa")); err != nil {
-		t.Fatal(err)
+	for _, val := range vals {
+		if err := ts.Insert(); err != nil {
+			t.Fatal(err)
+		}
+		if err := ts.SetVal("A", val); err != nil {
+			t.Fatal(err)
+		}
+		if err := idx.Insert(val, ts.RID()); err != nil {
+			t.Fatal(err)
+		}
 	}
 
-	ok, err := idx.Next()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !ok {
-		t.Fatal("no record found")
-	}
+	for _, val := range vals {
+		if err := idx.BeforeFirst(val); err != nil {
+			t.Fatal(err)
+		}
+		ok, err := idx.Next()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !ok {
+			t.Fatal("no record found")
+		}
 
-	gotRID, err := idx.DataRID()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !gotRID.Equals(ts.RID()) {
-		t.Fatalf("got %v, want %v", gotRID, ts.RID())
+		gotRID, err := idx.DataRID()
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Logf("val=%v, rid=%v", val, gotRID)
+		if err := ts.MoveToRID(gotRID); err != nil {
+			t.Fatal(err)
+		}
+		gotVal, err := ts.Val("A")
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !val.Equals(gotVal) {
+			t.Fatalf("expect %v, got %v", val, gotVal)
+		}
 	}
 
 	if err := idx.Close(); err != nil {
