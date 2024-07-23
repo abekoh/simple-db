26448fdbb895488783d97972ee6f60dc7ae1f570
Kotaro Abe <abekoh.bcky@gmail.com>
2024-07-23 16:34:30 +0900

    set command flag

--- cmd/simpledb/main.go
+++ cmd/simpledb/main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log"
 
 	"github.com/abekoh/simple-db/internal/postgres"
 )
 
 func main() {
+	var dir string
+	flag.StringVar(&dir, "dir", "", "directory to store data")
+	flag.Parse()
+
 	ctx := context.Background()
-	cfg := postgres.Config{}
+	cfg := postgres.Config{
+		Dir: dir,
+	}
 	if err := postgres.RunServer(ctx, cfg); err != nil {
 		log.Fatal(err)
 	}
