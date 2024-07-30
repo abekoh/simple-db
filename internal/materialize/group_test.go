b7ba42bae0f4c4364287dbe8d88724be51a27889
Kotaro Abe <abekoh.bcky@gmail.com>
2024-07-30 16:00:42 +0900

    test: fix

---
+++ internal/materialize/group_test.go
@@ -0,0 +1,85 @@
+package materialize_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/abekoh/simple-db/internal/materialize"
+	"github.com/abekoh/simple-db/internal/plan"
+	"github.com/abekoh/simple-db/internal/record"
+	"github.com/abekoh/simple-db/internal/record/schema"
+	"github.com/abekoh/simple-db/internal/simpledb"
+)
+
+func TestGroupByPlan(t *testing.T) {
+	ctx := context.Background()
+	db, err := simpledb.New(ctx, t.TempDir())
+	if err != nil {
+		t.Fatal(err)
+	}
+	tx, err := db.NewTx(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	sche := schema.NewSchema()
+	sche.AddInt32Field("A")
+	if err := db.MetadataMgr().CreateTable("mytable", sche, tx); err != nil {
+		t.Fatal(err)
+	}
+
+	layout := record.NewLayoutSchema(sche)
+	updateScan, err := record.NewTableScan(tx, "mytable", layout)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := updateScan.BeforeFirst(); err != nil {
+		t.Fatal(err)
+	}
+	for _, v := range []int32{1, 3, 12, 9, 7} {
+		if err := updateScan.Insert(); err != nil {
+			t.Fatal(err)
+		}
+		if err := updateScan.SetVal("A", schema.ConstantInt32(v)); err != nil {
+			t.Fatal(err)
+		}
+	}
+	if err := updateScan.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	tablePlan, err := plan.NewTablePlan("mytable", tx, db.MetadataMgr())
+	if err != nil {
+		t.Fatal(err)
+	}
+	groupByPlan := materialize.NewGroupByPlan(tx,
+		tablePlan,
+		[]schema.FieldName{"A"},
+		[]materialize.AggregationFunc{materialize.NewCountFunc("countA")},
+	)
+	projectPlan := plan.NewProjectPlan(groupByPlan, []schema.FieldName{"countA"})
+
+	queryScan, err := projectPlan.Open()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := queryScan.BeforeFirst(); err != nil {
+		t.Fatal(err)
+	}
+
+	ok, err := queryScan.Next()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !ok {
+		t.Fatal("no rows")
+	}
+	val, err := queryScan.Int32("countA")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if val != 5 {
+		t.Fatalf("got %d, want 5", val)
+	}
+}
